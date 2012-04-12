3a72902081f83c258da895aeb565a1e4893e6c95
mmaes <>
2012-04-12 10:42:17 +0000

    first iteration of 52X config files

---
+++ ConfigTemplates/PatTemplate_CMSSW_523_SampleVer_52X_SampleType_MCGenEvent_cfg.py
@@ -0,0 +1,294 @@
+# Starting with a skeleton process which gets imported with the following line
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+
+from PhysicsTools.PatAlgos.tools.coreTools import *
+
+process.source.fileNames = [
+"/store/relval/CMSSW_5_2_3/RelValTTbar/GEN-SIM-RECO/START52_V5-v1/0043/18E75EC8-2B7A-E111-B784-002354EF3BDE.root"
+]
+
+## import skeleton process
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+
+# load the PAT config
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+###############################
+####### Global Setup ##########
+###############################
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load("FWCore.Framework.test.cmsExceptionsFatal_cff")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.load("PhysicsTools.HepMCCandAlgos.genParticles_cfi")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.destinations = ['cerr']
+process.MessageLogger.statistics = []
+process.MessageLogger.fwkJobReports = []
+process.MessageLogger.categories=cms.untracked.vstring('FwkJob'
+							,'FwkReport'
+							,'FwkSummary'
+			                               )
+
+process.MessageLogger.cerr.INFO = cms.untracked.PSet(limit = cms.untracked.int32(0))
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10)
+process.options = cms.untracked.PSet(
+		                 wantSummary = cms.untracked.bool(True)
+	         	 	)
+
+process.GlobalTag.globaltag = cms.string('START52_V9::All')
+
+
+##-------------------- Import the Jet RECO modules ----------------------- ## this makes cmsRun crash
+##
+#process.load('RecoJets.Configuration.RecoPFJets_cff')
+##-------------------- Turn-on the FastJet density calculation -----------------------
+process.kt6PFJets.doRhoFastjet = True
+##-------------------- Turn-on the FastJet jet area calculation for your favorite algorithm -----------------------
+#process.ak5PFJets.doAreaFastjet = True
+
+process.primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
+                                           vertexCollection = cms.InputTag('offlinePrimaryVertices'),
+                                           minimumNDOF = cms.uint32(4) ,
+                                           maxAbsZ = cms.double(24), 
+                                           maxd0 = cms.double(2) 
+                                           )
+
+from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
+
+process.goodOfflinePrimaryVertices = cms.EDFilter(
+    "PrimaryVertexObjectFilter",
+    filterParams = pvSelector.clone( maxZ = cms.double(24.0) ),
+    src=cms.InputTag('offlinePrimaryVertices')
+    )
+
+############################### 
+########## Gen Setup ##########
+###############################  
+    
+#process.load("RecoJets.Configuration.GenJetParticles_cff")
+                
+process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
+
+############################### 
+########## Track Met ##########
+###############################  
+
+process.load("RecoMET.METProducers.trackMET_cff")
+from RecoMET.METProducers.trackMET_cff import *
+process.recoTrackMET = cms.Sequence(
+process.pfCandidatesForTrackMet*process.trackMet )
+                
+###############################
+####### PF2PAT Setup ##########
+###############################
+
+# Default PF2PAT with AK5 jets. Make sure to turn ON the L1fastjet stuff. 
+from PhysicsTools.PatAlgos.tools.pfTools import *
+postfix = "PF2PAT"
+usePF2PAT(process,runPF2PAT=True, jetAlgo="AK5", runOnMC=True, postfix=postfix)
+
+process.pfPileUpPF2PAT.Enable = True
+process.pfPileUpPF2PAT.Vertices = 'goodOfflinePrimaryVertices' #added
+process.pfJetsPF2PAT.doAreaFastjet = True
+process.pfJetsPF2PAT.doRhoFastjet = False
+
+process.pfIsolatedMuonsPF2PAT.combinedIsolationCut = cms.double(0.2)
+process.pfSelectedMuonsPF2PAT.cut = cms.string('pt > 10. && abs(eta) < 2.5')
+#process.pfMuonsFromVertexPF2PAT.vertices = cms.InputTag("goodOfflinePrimaryVertices")
+process.pfIsolatedElectronsPF2PAT.combinedIsolationCut = cms.double(0.2)
+process.pfSelectedElectronsPF2PAT.cut = cms.string('et > 15. && abs(eta) < 2.5')
+#process.pfElectronsFromVertexPF2PAT.vertices = cms.InputTag("goodOfflinePrimaryVertices")
+
+process.patJetCorrFactorsPF2PAT.payload = 'AK5PFchs'
+process.patJetCorrFactorsPF2PAT.levels = cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute'])
+process.patJetCorrFactorsPF2PAT.rho = cms.InputTag("kt6PFJetsPF2PAT", "rho")
+process.pfPileUpPF2PAT.checkClosestZVertex = False
+
+# Use the good primary vertices everywhere. 
+for imod in [process.patMuonsPF2PAT, process.patElectronsPF2PAT] :
+    imod.pvSrc = "goodOfflinePrimaryVertices"
+    imod.embedTrack = cms.bool(True)
+
+# top projections in PF2PAT:
+getattr(process,"pfNoPileUp"+postfix).enable = True
+getattr(process,"pfNoMuon"+postfix).enable = True
+getattr(process,"pfNoElectron"+postfix).enable = True
+getattr(process,"pfNoTau"+postfix).enable = False
+getattr(process,"pfNoJet"+postfix).enable = False
+
+# enable delta beta correction for muon selection in PF2PAT?
+getattr(process,"pfIsolatedMuons"+postfix).doDeltaBetaCorrection = False
+
+################################################################
+#### Clone the PF2PAT sequence for data-driven QCD estimate ####
+################################################################
+
+from PhysicsTools.PatAlgos.tools.helpers import cloneProcessingSnippet
+postfixNoLeptonCleaning = 'NoLeptonCleaning'
+
+# just cloning the first sequence, and enabling lepton cleaning 
+cloneProcessingSnippet(process, getattr(process, 'patPF2PATSequencePF2PAT'), postfixNoLeptonCleaning)
+
+getattr(process,"pfNoMuonPF2PATNoLeptonCleaning").enable = False
+getattr(process,"pfNoElectronPF2PATNoLeptonCleaning").enable = False 
+getattr(process,"pfIsolatedMuonsPF2PATNoLeptonCleaning").combinedIsolationCut = cms.double(999999)
+getattr(process,"pfIsolatedElectronsPF2PATNoLeptonCleaning").combinedIsolationCut = cms.double(999999)
+
+###############################
+### TagInfo and Matching Setup#
+###############################
+
+#process.patJetsPF2PAT.embedGenJetMatch = True
+#process.patJetsPF2PAT.getJetMCFlavour = True
+#process.patJetsPF2PAT.addGenPartonMatch = True
+# Add the calo towers and PFCandidates.
+process.patJetsPF2PAT.embedCaloTowers = True
+process.patJetsPF2PAT.embedPFCandidates = True
+process.patJetsPF2PAT.tagInfoSources = cms.VInputTag( cms.InputTag("secondaryVertexTagInfosAODPF2PAT") )
+
+###############################
+###### Bare KT 0.6 jets #######
+###############################
+
+from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets
+process.kt6PFJetsPF2PAT = kt4PFJets.clone(
+    rParam = cms.double(0.6),
+    src = cms.InputTag('pfNoElectronPF2PAT'),
+    doAreaFastjet = cms.bool(True),
+    doRhoFastjet = cms.bool(True)
+    )
+
+getattr(process,"patPF2PATSequencePF2PAT").replace( getattr(process,"pfNoElectronPF2PAT"), getattr(process,"pfNoElectronPF2PAT")*getattr(process,"kt6PFJetsPF2PAT") )
+
+###############################
+#### Selections Setup #########
+###############################
+
+# AK5 Jets
+#   PF
+process.selectedPatJetsPF2PAT.cut = cms.string("pt > 20")
+
+# Flavor history stuff
+process.load("PhysicsTools.HepMCCandAlgos.flavorHistoryPaths_cfi")
+process.flavorHistoryFilter.pathToSelect = cms.int32(-1)
+
+###############################
+#### TYPE 1 MET correction ####
+###############################
+
+#(/afs/cern.ch/user/l/lacroix/public/Mara/patTuple_42x_jec_cfg.py was recommended as example in the MET WorkBook)
+
+process.load("PhysicsTools.PatUtils.patPFMETCorrections_cff")
+
+  # for main PF2PAT products
+process.selectedPatJetsForMETtype1p2Corr.src = cms.InputTag('selectedPatJetsPF2PAT')
+process.selectedPatJetsForMETtype2Corr.src = cms.InputTag('selectedPatJetsPF2PAT')
+process.patPFJetMETtype1p2Corr.type1JetPtThreshold = cms.double(10.0)
+process.patPFJetMETtype1p2Corr.skipEM = cms.bool(False)
+process.patPFJetMETtype1p2Corr.skipMuons = cms.bool(False)
+
+  # for non lepton-iso PF2PAT
+
+cloneProcessingSnippet(process, getattr(process, 'producePatPFMETCorrections'), postfixNoLeptonCleaning)
+
+getattr(process,"selectedPatJetsForMETtype1p2Corr"+postfixNoLeptonCleaning).src = cms.InputTag('selectedPatJetsPF2PAT'+postfixNoLeptonCleaning)
+getattr(process,"selectedPatJetsForMETtype2Corr"+postfixNoLeptonCleaning).src = cms.InputTag('selectedPatJetsPF2PAT'+postfixNoLeptonCleaning)
+getattr(process,"patPFJetMETtype1p2Corr"+postfixNoLeptonCleaning).type1JetPtThreshold = cms.double(10.0)
+getattr(process,"patPFJetMETtype1p2Corr"+postfixNoLeptonCleaning).skipEM = cms.bool(False)
+getattr(process,"patPFJetMETtype1p2Corr"+postfixNoLeptonCleaning).skipMuons = cms.bool(False)
+
+# let it run
+
+process.patseq = cms.Sequence(
+		process.kt6PFJets*
+                process.goodOfflinePrimaryVertices* 
+                process.primaryVertexFilter * #removes events with no good pv (but if cuts to determine good pv change...)
+                getattr(process,"patPF2PATSequence"+postfix)* # main PF2PAT
+                getattr(process,"patPF2PATSequence"+postfix+postfixNoLeptonCleaning)* # PF2PAT FOR DATA_DRIVEN QCD
+                process.patDefaultSequence*
+		process.flavorHistorySeq*
+                process.makeGenEvt
+                )
+
+####################################
+#### ENERGY CONSERVATION FILTER ####
+####################################
+
+process.totalKinematicsFilter = cms.EDFilter('TotalKinematicsFilter',
+		  src             = cms.InputTag("genParticles"),
+		    MHTSource       = cms.InputTag("mhtPF"),
+		      tolerance       = cms.double(0.5),
+		        verbose         = cms.untracked.bool(False)
+			)
+
+#################
+#### ENDPATH ####
+#################
+
+process.p = cms.Path(
+        #process.totalKinematicsFilter*process.recoTrackMET*process.patseq*process.producePatPFMETCorrections
+        process.totalKinematicsFilter*process.patseq*process.producePatPFMETCorrections*getattr(process, 'producePatPFMETCorrections'+postfixNoLeptonCleaning)
+    )
+
+process.out.SelectEvents.SelectEvents = cms.vstring('p')
+    
+# rename output file
+process.out.fileName = "TTJets_Madgraph_Summer11_Final1_PAT.root"
+
+
+# reduce verbosity
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+
+
+# process all the events
+process.maxEvents.input = -1 #changed
+process.options.wantSummary = False
+process.out.dropMetaData = cms.untracked.string("DROPPED")
+
+process.source.inputCommands = cms.untracked.vstring("keep *", "drop *_MEtoEDMConverter_*_*")
+
+process.out.outputCommands = [
+    'drop *_cleanPat*_*_*',
+    'keep *_selectedPat*_*_*',
+    'keep *_goodPat*_*_*',
+    'keep patJets_*_*_*',
+    'keep *_patMETs*_*_*',
+    'keep *_patType1CorrectedPFMet*_*_*',
+    'keep *_goodOfflinePrimaryVertices*_*_*',    
+    'drop patPFParticles_*_*_*',
+    'drop patTaus_*_*_*',
+    'keep double_*PF2PAT*_*_PAT',
+    'keep *_TriggerResults_*_*',
+    'keep *_hltTriggerSummaryAOD_*_*',
+    'keep *_ak5GenJets*_*_*',
+    'keep double_genEvent*_*_*',
+    'keep TtGenEvent_*_*_*',
+    'keep GenRunInfoProduct_generator_*_*',
+    'keep GenEventInfoProduct_generator_*_*',
+    'keep *_genEventWeight_*_*',
+    'keep *_bFlavorHistoryProducer_*_*',
+    'keep *_cFlavorHistoryProducer_*_*',
+    'keep *_flavorHistoryFilter_*_*',
+    'keep PileupSummaryInfos_*_*_*',
+    'keep recoTracks_generalTracks_*_*',
+    'keep recoPFCandidates_selectedPatJets*_*_*',
+    'keep recoBaseTagInfosOwned_selectedPatJets*_*_*',
+    'keep CaloTowers_selectedPatJets*_*_*',
+    'keep *_*genParticles*_*_*',
+    'keep recoGenParticles_*_*_*',
+    'keep *_offlineBeamSpot_*_*',
+    'keep recoCaloClusters_*_*_*',
+    'keep edmHepMC*_*_*_*',
+    'keep recoPdfinfo_*_*_*',
+    'keep triggerTriggerEvent_*_*_*',
+    'keep GenEventInfoProduct_*_*_*',
+    'keep DcsStatuss_scalersRawToDigi__*',
+    'drop *_selectedPatJets_*_*',
+    'drop *_patMETs_*_*',
+    'drop *_selectedPatPhotons*_*_*'
+    ]
+
